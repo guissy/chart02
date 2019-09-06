52a44b5715d4c95cb47d1cb1d173fee357a96667
桂子 <guissy@qq.com>
2019-09-07 01:45:49 +0800

    nice line

---
+++ src/bg/NiceLine.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="nice-line">
+        <img src="../assets/line_circle4.svg" />
+    </div>
+</template>
+
+<style scoped>
+    .nice-line {
+        position: absolute;
+        bottom: 5px;
+        left: 3px;
+        opacity: 0.7;
+    }
+    .nice-line > img {
+        width: 340px;
+    }
+</style>
