0ca3910c04461d5c7b340b0dce24524cbd27f37f
桂子 <guissy@qq.com>
2019-09-19 10:16:45 +0800

    storybook for vue

---
+++ stories/index.stories.js
@@ -0,0 +1,53 @@
+import {storiesOf} from '@storybook/vue';
+// import { linkTo } from '@storybook/addon-links';
+import CircleArc from '../src/bg/CircleArc';
+import NiceLine from '../src/bg/NiceLine';
+import DotLight from '../src/earth/DotLight';
+import DotLightLow from '../src/earth/DotLightLow';
+import DoubleLightButton from '../src/earth/DoubleLightButton';
+import Earth from '../src/earth/Earth';
+import Light from '../src/earth/Light';
+import Line from '../src/earth/LineArc';
+import TripleLightButton from '../src/earth/TripleLightButton';
+import {themes} from '@storybook/theming';
+
+storiesOf('bg', module)
+  .add('with CircleArc', () => ({
+    components: {CircleArc},
+    template: '<CircleArc/>',
+  }))
+  .add('with NiceLine', () => ({
+    components: {NiceLine},
+    template: '<NiceLine/>',
+  }))
+
+storiesOf('earth', module)
+  .addParameters({
+    options: {
+      theme: themes.dark,
+    },
+  })
+  .add('with DotLight', () => ({
+    components: {DotLight},
+    template: '<DotLight :style="{position: \'absolute\', top: \'50px\', left: \'50px\'}" />',
+  }))
+  .add('with DotLightLow', () => ({
+    components: {DotLightLow},
+    template: '<DotLightLow />'
+  }))
+  .add('with DoubleLightButton', () => ({
+    components: {DoubleLightButton},
+    template: '<DoubleLightButton text="ok" value="30p" :style="{ transform: \'\' }"/>'
+  }))
+  .add('with Light', () => ({
+    components: {Light},
+    template: '<Light :pos="[20, 20, 50, 50]"/>'
+  }))
+  .add('with TripleLightButton', () => ({
+    components: {TripleLightButton},
+    template: '<TripleLightButton text="ok" value="30p"/>'
+  }))
+  .add('with Earth', () => ({
+    components: {Earth},
+    template: '<Earth/>'
+  }));
